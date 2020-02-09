413d7e5e529306062fed33627ed8fd9b13eeb387
RowickaM <rowickam.23@gmial.com>
2020-02-09 12:36:56 +0100

    javadoc + drobna zmiana kosmetyczna dla wyglądu głównego ekranu

--- app/src/main/java/com/rowicka/gitartuner/utility/CheckConnection.java
+++ app/src/main/java/com/rowicka/gitartuner/utility/CheckConnection.java
@@ -13,20 +13,36 @@ public class CheckConnection {
     private Context context;
     private NetworkInfo activeNetwork;
 
+    /**
+     * Konstruktoir klasy CheckConnection ustawiający kontekst aplikacji
+     * @param context aktualny kontekst aplikacji
+     */
     public CheckConnection(Context context) {
         this.context = context;
         setInstanceOfNetwork();
     }
 
+    /**
+     * Funkcja dodająca do activeNetwork aktualne informacje o sieci
+     */
     private void setInstanceOfNetwork() {
         ConnectivityManager cm = (ConnectivityManager) this.context.getSystemService(Context.CONNECTIVITY_SERVICE);
         this.activeNetwork = cm.getActiveNetworkInfo();
     }
 
+    /**
+     * Funkcja sparawdzająca połączenie z Internetem
+     * @return trur - jeśli połączenie z Internetem istnieje false - jeśli połączenie z Internetem nie istnieje
+     */
     public boolean isConnected() {
         return this.activeNetwork != null && this.activeNetwork.isConnectedOrConnecting();
     }
 
+    /**
+     * Funkcja tworząca okno informujące o braku interetu oraz pozwalająca na ponowne sprawdzenie statusu połączenia
+     * oraz na przejście do ustawień
+     * @return obiekt typu AlertDialog.Builer zawierający zachowanie i wygląd okna dialogowego informującego o braku dostępu do sieci
+     */
     public AlertDialog.Builder notConnectedAlertDialog() {
         final AlertDialog.Builder dialog = new AlertDialog.Builder(context)
                 .setTitle("Brak połączenia")
